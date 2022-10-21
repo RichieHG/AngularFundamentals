32fd00794108bf8bb38faa6576948d00561ddd57
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-21 14:03:42 -0500

    Feat: Add First component, NgModule and Bootstrapping

--- AngularFundamentals/src/main.ts
+++ AngularFundamentals/src/main.ts
@@ -1,48 +1,40 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import { upperCase } from './introduction/formatter';
+import { AppModule } from "./app/app.module";
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
 
 //Public Mehtod using PROTOTYPE
 // ShoppingList.prototype.addItem = function (item) {
 //   this.groceries = this.groceries.concat([item]);
 // }
 
-class ShoppingList2 {
-    groceries : string [] = [];
-    constructor() {
-        this.groceries = [];
-    }
+// class ShoppingList2 {
+//     groceries : string [] = [];
+//     constructor() {
+//         this.groceries = [];
+//     }
 
-    addItem(item: string){
-        this.groceries = [... this.groceries, item];
-    }
+//     addItem(item: string){
+//         this.groceries = [... this.groceries, item];
+//     }
 
-    removeItem(item: string){
-        this.groceries = this.groceries.filter( (grocery) => item !== grocery);
-    }
-}
+//     removeItem(item: string){
+//         this.groceries = this.groceries.filter( (grocery) => item !== grocery);
+//     }
+// }
 
-const myNewList = new ShoppingList2();
-myNewList.addItem('Pear');
-myNewList.addItem('Pizza');
-myNewList.addItem('Water');
+// const myNewList = new ShoppingList2();
+// myNewList.addItem('Pear');
+// myNewList.addItem('Pizza');
+// myNewList.addItem('Water');
 
-console.log(myNewList.groceries);
+// console.log(myNewList.groceries);
 
-myNewList.removeItem('Pear');
+// myNewList.removeItem('Pear');
 
-console.log(myNewList);
+// console.log(myNewList);
 
 
-let name : string = "Neftali Hernandez";
-console.log(upperCase(name));
+// let name = "Neftali Hernandez";
+// console.log(upperCase(name));
