c7a298b18de955ea735c9d669a517c94df56555f
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-24 18:42:19 -0500

    Feat: Add examples about Pipes and SafeNavigation

--- AngularFundamentals/src/app/app.component.ts
+++ AngularFundamentals/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 interface Passenger{
   id: number,
   fullName: string,
-  checkedIn: boolean
+  checkedIn: boolean,
+  checkInDate: number | null,
+  children: Child[] | null
+}
+
+interface Child{
+  name: string,
+  age: number
 }
 
 @Component({
@@ -29,22 +36,31 @@ export class AppComponent {
     {
       id: 1,
       fullName: "Ricardo",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1460842000000,
+      children: [{name: "Rosa", age: 12}]
     },
     {
       id: 2,
       fullName: "Ricardo2",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1410842000000,
+      children: null
     },
     {
       id: 3,
       fullName: "Ricardo3",
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null,
+      children: [{name: "Luisa", age: 12}]
+
     },
     {
       id: 4,
       fullName: "Ricardo4",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1890722000000,
+      children: null
     }
   ]
   constructor() {
