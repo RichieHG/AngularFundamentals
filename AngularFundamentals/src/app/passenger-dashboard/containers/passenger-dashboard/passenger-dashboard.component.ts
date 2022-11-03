edbf7d03427ec2bdc9a85f2567182c47a6e13e91
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-11-03 15:45:34 -0600

    Feat: Add examples about ngOnInit, InputData, PresentationalComponents, OutputEventEmitter, InmutableState and ngOnChanges

--- AngularFundamentals/src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ AngularFundamentals/src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { Passenger } from "src/app/models/passenger.interface";
+import { Component, OnInit } from "@angular/core";
+import { Passenger } from "src/app/passenger-dashboard/models/passenger.interface";
 
 
 @Component({
@@ -8,38 +8,54 @@ import { Passenger } from "src/app/models/passenger.interface";
   templateUrl: 'passenger-dashboard.component.html'
 })
 
-export class PassengerDashboardComponent{
+export class PassengerDashboardComponent implements OnInit {
+  passengers: Passenger[] = [];
 
-  passengers: Passenger[] = [
-    {
-      id: 1,
-      fullName: "Ricardo",
-      checkedIn: true,
-      checkInDate: 1460842000000,
-      children: [{name: "Rosa", age: 12}]
-    },
-    {
-      id: 2,
-      fullName: "Ricardo2",
-      checkedIn: true,
-      checkInDate: 1410842000000,
-      children: null
-    },
-    {
-      id: 3,
-      fullName: "Ricardo3",
-      checkedIn: false,
-      checkInDate: null,
-      children: [{name: "Luisa", age: 12}]
+  constructor() { }
+  ngOnInit() {
+    this.passengers = [
+      {
+        id: 1,
+        fullName: "Ricardo",
+        checkedIn: true,
+        checkInDate: 1460842000000,
+        children: [{ name: "Rosa", age: 12 }]
+      },
+      {
+        id: 2,
+        fullName: "Ricardo2",
+        checkedIn: true,
+        checkInDate: 1410842000000,
+        children: null
+      },
+      {
+        id: 3,
+        fullName: "Ricardo3",
+        checkedIn: false,
+        checkInDate: null,
+        children: [{ name: "Luisa", age: 12 }]
 
-    },
-    {
-      id: 4,
-      fullName: "Ricardo4",
-      checkedIn: true,
-      checkInDate: 1890722000000,
-      children: null
-    }
-  ]
+      },
+      {
+        id: 4,
+        fullName: "Ricardo4",
+        checkedIn: true,
+        checkInDate: 1890722000000,
+        children: null
+      }
+    ]
+  }
 
+  handleRemove(event: Passenger){
+    this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id);
+  }
+
+  handleEdit(event: Passenger){
+    this.passengers = this.passengers.map((passsenger: Passenger) => {
+      if(passsenger.id === event.id){
+        passsenger = Object.assign({}, passsenger, event);
+      }
+      return passsenger;
+    });
+  }
 }
